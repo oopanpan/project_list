2633a4a77258e0740cb2f9473ac85dab272a5fe8
oopanpan <oopanpan@gmail.com>
2021-07-31 13:16:59 -0700

    main deliverable finished

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,54 +1,113 @@
 import React, { useState, useEffect } from 'react';
-import { Input } from 'antd';
+import { Input, Col, Row, Modal } from 'antd';
 
 import projectIcon from '../assets/defaultProjectIcon_2x.png';
 import editIcon from '../assets/EditIcon.svg';
 import editIconHover from '../assets/EditIcon_Hover.svg';
 import deleteIcon from '../assets/DeleteIcon.svg';
 import deleteIconHover from '../assets/DeleteIcon_Hover.svg';
+import questionSign from '../assets/Question.svg';
 
-function ProjectItem({ project, id, handleEditingProject, handleDelete }) {
+import './ProjectItem.css';
+import { float } from 'webidl-conversions';
+
+function ProjectItem({ project, index, handleEditingProject, handleDelete }) {
 	const [editing, setEditing] = useState(project.editing);
 	const [title, setTitle] = useState(project.title);
+	const [editIconSrc, setEditIconSrc] = useState(editIcon);
+	const [deleteIconSrc, setDeleteIconSrc] = useState(deleteIcon);
+	const [modalVisible, setModalVisible] = useState(false);
 
 	useEffect(() => {
 		setEditing(project.editing);
 	}, [project]);
 
+	const handleModalOk = () => {
+		handleDelete(index);
+		setModalVisible(false);
+	};
+
 	return (
-		<div className='project-item' id={id} draggable>
-			<img alt='project-icon' src={projectIcon} />
+		<Row
+			justify='start'
+			align='middle'
+			className='project-item-row'
+			draggable
+		>
+			<Col xs={{ span: 4 }} sm={{ span: 4 }}>
+				<img alt='project-icon' src={projectIcon} />
+			</Col>
 			{editing ? (
-				<Input
-					type='text'
-					placeholder='Name your project'
-					value={title}
-					onChange={(event) => setTitle(event.target.value)}
-					onBlur={(event) => handleEditingProject(event, title)}
-					onPressEnter={(event) => handleEditingProject(event, title)}
-					autoFocus
-				/>
+				<Col xs={{ span: 8 }} sm={{ span: 8 }}>
+					<Input
+						type='text'
+						placeholder='Name your project'
+						value={title}
+						onChange={(event) => setTitle(event.target.value)}
+						onBlur={(event) =>
+							handleEditingProject(event, title, index)
+						}
+						onPressEnter={(event) =>
+							handleEditingProject(event, title, index)
+						}
+						autoFocus
+					/>
+				</Col>
 			) : (
 				<>
-					<div>{project.title}</div>
-					<img
-						onClick={() => setEditing(true)}
-						alt='edit'
-						src={editIcon}
-					/>
+					<Col xs={{ span: 8 }} sm={{ span: 6 }}>
+						<p>{project.title}</p>
+					</Col>
+					<Col xs={{ span: 2 }} sm={{ span: 2 }}>
+						<img
+							style={{ float: 'right' }}
+							src={editIconSrc}
+							onClick={() => setEditing(true)}
+							onMouseEnter={() => setEditIconSrc(editIconHover)}
+							onMouseLeave={() => setEditIconSrc(editIcon)}
+							alt='edit'
+						/>
+					</Col>
 				</>
 			)}
 			{project.date && (
 				<>
-					<div>{project.date}</div>
-					<img
-						alt='delete'
-						onClick={(event) => handleDelete(event)}
-						src={deleteIcon}
-					/>
+					<Col xs={{ span: 0 }} sm={{ span: 10 }}>
+						<p>{project.date}</p>
+					</Col>
+					<Col xs={{ span: 2 }} sm={{ span: 2 }}>
+						<img
+							alt='delete'
+							onClick={() => setModalVisible(true)}
+							onMouseEnter={() =>
+								setDeleteIconSrc(deleteIconHover)
+							}
+							onMouseLeave={() => setDeleteIconSrc(deleteIcon)}
+							src={deleteIconSrc}
+						/>
+					</Col>
 				</>
 			)}
-		</div>
+			<Modal
+				title={
+					<>
+						<img
+							alt='question mark'
+							src={questionSign}
+							style={{ width: '30px', height: '30px' }}
+						/>
+						<p>Are you sure you want to delete this project</p>
+					</>
+				}
+				visible={modalVisible}
+				okText='Yes'
+				cancelText='No'
+				onOk={handleModalOk}
+				onCancel={() => setModalVisible(false)}
+			>
+				<p>This action can't be undone</p>
+			</Modal>
+		</Row>
 	);
 }
 
--- src/containers/ProjectListContainer.js
+++ src/containers/ProjectListContainer.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
-import CollectionTitle from '../components/CollectionTitle';
+import React, { useState, useEffect } from 'react';
 
+import { Row } from 'antd';
+import CollectionTitle from '../components/CollectionTitle';
 import ProjectItem from '../components/ProjectItem';
 import NewProjectButton from '../components/NewProjectButton';
 
+import './ProjectListContainer.css';
+
 function ProjectListContainer() {
 	const [projectList, setProjectList] = useState([]);
+	const [projectGroupClassName, setProjectGroupClassName] = useState(
+		'project-entry-group'
+	);
 
 	// Here we can have a useEffect to fetch projectList from the database to
+	useEffect(() => {
+		if (window.innerWidth <= 576) {
+			setProjectGroupClassName('project-entry-group-mobile');
+		} else {
+			setProjectGroupClassName('project-entry-group');
+		}
+	}, [window.innerWidth]);
 
 	const renderProjects = () => {
 		return projectList.map((project, index) => (
 			<ProjectItem
-				id={index}
+				index={index}
 				key={index}
 				project={project}
 				handleEditingProject={handleEditingProject}
@@ -30,26 +43,24 @@ function ProjectListContainer() {
 		setProjectList(newProjectList);
 	};
 
-	const handleEditingProject = (event, title) => {
+	const handleEditingProject = (event, title, index) => {
 		event.stopPropagation();
-		const targetedIndex = event.target.parentNode.id;
+		let [, month, day, year, time] = Date().split(' ');
+		let [hour, minute] = time.split(':');
 		const editedProject = {
-			...projectList[targetedIndex],
+			...projectList[index],
 			title: title,
-			date: Date(),
+			date: `${month} ${day},${year}  ${hour}:${minute}`,
 			editing: false,
 		};
 		const updatedProjectListArray = [...projectList];
-		updatedProjectListArray.splice(targetedIndex, 1, editedProject);
+		updatedProjectListArray.splice(index, 1, editedProject);
 		setProjectList(updatedProjectListArray);
 	};
 
-	const handleDelete = (event) => {
-		event.stopPropagation();
-		const targetedIndex = event.target.parentNode.id;
-		console.log(targetedIndex);
+	const handleDelete = (index) => {
 		const updatedProjectListArray = [...projectList];
-		updatedProjectListArray.splice(targetedIndex, 1);
+		updatedProjectListArray.splice(index, 1);
 		setProjectList(updatedProjectListArray);
 	};
 
@@ -57,7 +68,7 @@ function ProjectListContainer() {
 		<div className='project-container'>
 			<CollectionTitle />
 			<NewProjectButton addNewProject={addNewProject} />
-			<div>{renderProjects()}</div>
+			<div className={projectGroupClassName}>{renderProjects()}</div>
 		</div>
 	);
 }
